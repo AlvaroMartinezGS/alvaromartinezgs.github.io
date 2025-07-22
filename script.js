fe22beb7812788543ecc8957779ac83f0f88ec34
AlvaroMartinezGS <130599830+AlvaroMartinezGS@users.noreply.github.com>
2025-07-22 13:50:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Portfolio v5 cargado');
+const toggleBtn = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
+document.querySelectorAll('.sidebar nav a').forEach(link => {
+  link.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+  });
+});
